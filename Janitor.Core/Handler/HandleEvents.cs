923ee29c1389b9b954973cca0a65e30d62e27fa2
Zytrel <zytrel@gmx.com>
2024-07-01 19:27:20 +0200

    Don't announce on every reconnect.

--- Janitor.Core/Handler/HandleEvents.cs
+++ Janitor.Core/Handler/HandleEvents.cs
@@ -71,8 +71,10 @@ namespace Janitor.Handler
             foreach (var guild in guilds)
             {
                 Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {guild.Name}: Janitor Bot v{BotVersion} ready.");
+#if DEBUG
                 LogMessage(guild.Id, $"Janitor Bot v{BotVersion} ready.", InformationType.Information, ResponseMessageType.BotReady);
-                
+#endif
+
                 // Create essential roles when client is ready.
                 await GetOrCreateRole(guild, roleFriend);
                 await GetOrCreateRole(guild, roleManager);
